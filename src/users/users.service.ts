0802835dec206c96247080037baf8ee2f2f1b2ec
Mehdi <mehdi@ublo.immo>
2022-01-07 11:40:11 +0100

    [Add]: hardcoded user list

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,4 +4,18 @@ import { Injectable } from '@nestjs/common';
 export type User = any;
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+}
